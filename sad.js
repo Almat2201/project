5e382f9e102b113262c907d46997f5d2de110c4e
unknown <almat011202@gmail.com>
2023-06-14 14:46:44 +0500

    first commit

---
+++ sad.js
@@ -0,0 +1,50 @@
+"use strict";
+
+//let number = 4;
+
+//console.log(-4/0);
+//console.log("string" * 9);
+
+//const persone = "5";
+
+//const bool =false; 
+
+//console.log(something);
+
+//let und;
+//console.log(und);
+
+//const obj = {
+    name: "John",
+    age: 25,
+    cock: false
+};
+
+//console.log(obj.name);
+//console.log(obj.cock);
+
+//let arr = ["plum.png", "orange.png", 6, {}, [], "apple.png"];
+//console.log(arr[1]);
+
+//const arr = ["a", "b", "c"];
+//arr[10] = "2312";
+
+
+//console.log(arr);
+
+///const arrObj = {
+    a: "a",
+    1: "b",
+    2: "c",
+    abc: {
+        df: [{}, {}],
+        def: {
+
+        }
+    }
+};
+//const b = "b";
+
+//arrObj.b = "1234";
+//arrObj["b"] = {};
+//console.log(arrObj.b);
---
+++ переменные.js
@@ -0,0 +1,17 @@
+"use strict";
+
+//alert("Hello!");
+
+//const result = confitm("Are you here");
+//console.log(result);
+
+//const answer = +promrt("Вам есть 18?", "10");
+//console.log(answer + 5));
+
+const answers = []
+
+answers[0] = prompt("как ваше имя?");
+answers[1] = prompt("как ваше фамилия?");
+answers[2] = prompt("Сколько вам лет?");
+
+document.write(answers);
